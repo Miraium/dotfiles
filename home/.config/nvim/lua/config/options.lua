8708c29c34dcbeeae2f1b40fce4e566893aeefce
miraium <ognek.m@gmail.com>
2026-02-22 22:01:23 +0900

    Tune nvim indentation defaults and show hidden files in explorer

--- home/.config/nvim/lua/config/options.lua
+++ home/.config/nvim/lua/config/options.lua
@@ -1,3 +1,16 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+
+vim.opt.tabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.softtabstop = 4
+vim.opt.expandtab = true
+
+-- Visualize whitespace to make tabs/spaces differences explicit while editing.
+vim.opt.list = true
+vim.opt.listchars = {
+  tab = ">-",
+  trail = ".",
+  nbsp = "+",
+}
---
+++ home/.config/nvim/lua/plugins/snacks_explorer.lua
@@ -0,0 +1,13 @@
+return {
+  {
+    "folke/snacks.nvim",
+    opts = function(_, opts)
+      opts = opts or {}
+      opts.picker = opts.picker or {}
+      opts.picker.sources = opts.picker.sources or {}
+      opts.picker.sources.explorer = vim.tbl_deep_extend("force", opts.picker.sources.explorer or {}, {
+        hidden = true,
+      })
+    end,
+  },
+}
